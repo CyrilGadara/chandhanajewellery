07a21c972034f19e1d035c78720c957290e76619
CyrilGadara <gadaracyril11@gmail.com>
2021-12-11 09:04:54 +0530

    Adding Required Files

---
+++ assets/scripts/script.js
@@ -0,0 +1,24 @@
+console.log('Script Working');
+
+const burger = document.querySelector('.burger')
+const mobileNav = document.querySelector('.header__mobile');
+console.log(mobileNav)
+let menuOpen = false;
+burger.addEventListener('click',()=>{
+    if(!menuOpen){
+        burger.classList.add('open');
+        mobileNav.classList.add('visible')
+        menuOpen = true;
+    }else{
+        burger.classList.remove('open');
+        mobileNav.classList.remove('visible')
+        menuOpen = false;
+    }
+})
+
+
+lightGallery(document.getElementById('lightgallery'),{
+    autplay: true,
+    progressBar: true,
+    download: false
+});
